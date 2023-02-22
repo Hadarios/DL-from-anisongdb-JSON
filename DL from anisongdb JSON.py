2d44e6862e980cbc5510c6a306f32b3fe33a7249
Hadarios <nolan.djellal@gmail.com>
2023-02-22 02:07:25 -0500

    added utf-8 to id3

--- DL from anisongdb JSON.py
+++ DL from anisongdb JSON.py
@@ -99,7 +99,7 @@ for L in source:
 
         audiofile.tag.artist = L['songArtist']
         audiofile.tag.title = L['songName'] + " (" + anime + " - " + type + ")"
-        audiofile.tag.save(version=(2,3,0))
+        audiofile.tag.save(version=(2,3,0), encoding="utf-8")
         print("Added")
         progress+=1
         print("{0} complete, to next song! {1}/{2} ({3}%)\n".format(name, progress, len(source), round(progress/len(source)*100), 2))
